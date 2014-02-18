b1b1b86a30b69cd99edb1b30aef7a7a20d95e512
joe <jemoster@gmail.com>
2014-02-17 22:05:42 -0800

    Fixed game logic and added test maps

--- conway/support.go
+++ conway/support.go
@@ -8,69 +8,79 @@ import (
 )
 
 type Game struct {
-	Map []byte
+	Map  []byte
 	Rows int
 	Cols int
 }
 
-func  (game *Game) index(row int, col int) (i int){
-	if(row<0){ row = game.Rows-1 }
-	if(col<0){ col = game.Cols-1 }
-	if(row>game.Rows-1){ row = 0 }
-	if(col>game.Cols-1){ col = 0 }
+func (game *Game) index(row int, col int) (i int) {
+	if row < 0 {
+		row = game.Rows - 1
+	}
+	if col < 0 {
+		col = game.Cols - 1
+	}
+	if row > game.Rows-1 {
+		row = 0
+	}
+	if col > game.Cols-1 {
+		col = 0
+	}
 
-	i = game.Rows*row + col
+	i = game.Cols*row + col
 	return i
 }
 
 func (game *Game) Print() {
-	for i := 0; i < game.Cols; i++ {
-		for j := 0; j < game.Rows; j++ {
-			fmt.Printf("%c", game.Map[i*game.Cols+j])
+	fmt.Printf("\033[1;1H")
+	for row := 0; row < game.Rows; row++ {
+		for col := 0; col < game.Cols; col++ {
+			fmt.Printf("%c", game.Map[game.index(row, col)])
 		}
 		fmt.Printf("\n")
 	}
 }
 
 func (game *Game) Step() {
-	for row:=0; row<game.Rows; row++ {
-		for col:=0; col<game.Cols; col++{
+	for row := 0; row < game.Rows; row++ {
+		for col := 0; col < game.Cols; col++ {
 			//Get live neighbors
-			live := 0;
-			live += isLive(game.Map[game.index(row-1,col-1)])
-			live += isLive(game.Map[game.index(row-1,col)])
-			live += isLive(game.Map[game.index(row-1,col+1)])
-			live += isLive(game.Map[game.index(row,col-1)])
-			live += isLive(game.Map[game.index(row,col-1)])
-			live += isLive(game.Map[game.index(row+1,col-1)])
-			live += isLive(game.Map[game.index(row+1,col)])
-			live += isLive(game.Map[game.index(row+1,col+1)])
-			
+			live := 0
+			live += isLive(game.Map[game.index(row-1, col-1)])
+			live += isLive(game.Map[game.index(row-1, col)])
+			live += isLive(game.Map[game.index(row-1, col+1)])
+			live += isLive(game.Map[game.index(row, col-1)])
+			live += isLive(game.Map[game.index(row, col+1)])
+			live += isLive(game.Map[game.index(row+1, col-1)])
+			live += isLive(game.Map[game.index(row+1, col)])
+			live += isLive(game.Map[game.index(row+1, col+1)])
+
 			//Apply rules
-			if(isLive(game.Map[game.index(row,col)])==1){
-				if(live<2 || live>3){
-					game.Map[game.index(row,col)] = 'd'
+			if isLive(game.Map[game.index(row, col)]) == 1 {
+				if live < 2 || live > 3 {
+					game.Map[game.index(row, col)] = 'd'
 				}
-				
-			} else if(live==3) {
-				game.Map[game.index(row,col)] = 'n'	
+
+			} else if live == 3 && game.Map[game.index(row, col)] != 'r' {
+				game.Map[game.index(row, col)] = 'n'
 			}
+			//game.Map[game.index(row,col)] = 48+byte(live)
 		}
 	}
-	for row:=0; row<game.Rows; row++ {
-		for col:=0; col<game.Cols; col++{
-			if(game.Map[game.index(row,col)]=='d') {
-				game.Map[game.index(row,col)] = '_'
-			} else if (game.Map[game.index(row,col)]=='n'){
-				game.Map[game.index(row,col)] = 'r'
+	for row := 0; row < game.Rows; row++ {
+		for col := 0; col < game.Cols; col++ {
+			if game.Map[game.index(row, col)] == 'd' {
+				game.Map[game.index(row, col)] = '_'
+			} else if game.Map[game.index(row, col)] == 'n' {
+				game.Map[game.index(row, col)] = 'r'
 			}
 		}
 	}
 }
 func isLive(cell byte) (live int) {
 	live = 0
-	if(cell=='r' || cell=='d'){
-		 live = 1
+	if cell == 'r' || cell == 'd' {
+		live = 1
 	}
 	return
 }
@@ -90,18 +100,19 @@ func (game *Game) FromFile(name string) (err error) {
 }
 
 func (game *Game) Load(r *bufio.Reader) (err error) {
-	game.Rows = 0;
-	game.Cols = 0;
-	for ;; {
+	game.Rows = 0
+	game.Cols = 0
+	for {
 		line, err := r.ReadString('\n')
 		if err == io.EOF {
 			break
 		} else if err != nil {
 			fmt.Printf("Error: %s\n", err)
 		}
-		game.Map = append(game.Map,line[0:len(line)-1]...)
+		game.Map = append(game.Map, line[0:len(line)-1]...)
 		game.Rows++
-		game.Cols = len(line)-1
+		game.Cols = len(line) - 1
 	}
+	//fmt.Println(game.Map)
 	return
 }
--- runConway.go
+++ runConway.go
@@ -4,15 +4,17 @@ import (
 	"./conway"
 	"fmt"
 	"os"
+	"strconv"
+	"time"
 )
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "usage: %s [*.cw]\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "usage: %s [*.cw] [Step limit (-1 for none)]\n", os.Args[0])
 	os.Exit(2)
 }
 
 func main() {
-	if len(os.Args) != 2 {
+	if len(os.Args) != 3 {
 		usage()
 	}
 
@@ -23,10 +25,17 @@ func main() {
 		fmt.Printf("Map failed to load, Error: %s\n", err)
 	}
 
+	stepLimit, err := strconv.ParseInt(os.Args[2], 10, 64)
+	if err != nil {
+		fmt.Println("Invalid step limit, Error: %s", err)
+	}
+
 	game.Print()
-	for i:=0; i<10; i++ {
-		fmt.Println("----------")
-		game.Step();
+	fmt.Println("Rows:%s", game.Rows)
+	fmt.Println("Cols:%s", game.Cols)
+	for i := 0; i < int(stepLimit); i++ {
+		time.Sleep(200 * time.Millisecond)
+		game.Step()
 		game.Print()
 	}
 }
